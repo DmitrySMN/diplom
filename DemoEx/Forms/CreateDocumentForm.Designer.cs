da009792814cf9572be7703678239413cbf177f5
Dmitry Semenov <ddmtry064@gmail.com>
2025-05-05 16:43:03 +0300

    feat: add the ability to create documents

--- DemoEx/Forms/CreateDocumentForm.Designer.cs
+++ DemoEx/Forms/CreateDocumentForm.Designer.cs
@@ -70,6 +70,7 @@
             this.button2.TabIndex = 0;
             this.button2.Text = "Договор купли-продажи";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
--- DemoEx/Forms/CreateDocumentForm.cs
+++ DemoEx/Forms/CreateDocumentForm.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using DemoEx.data;
+using DemoEx.utility;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DB;
 
 namespace DemoEx.Forms
 {
     public partial class CreateDocumentForm : Form
     {
-        private int dealId; 
+        private int dealId;
+        private Db db = new Db(Connection.connectionString);
         public CreateDocumentForm(int dealId = 0)
         {
             InitializeComponent();
@@ -27,6 +32,25 @@ namespace DemoEx.Forms
         private void CreateDocumentForm_Load(object sender, EventArgs e)
         {
             groupBox1.Text = $"Сделка №{dealId}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DocumentHelper.generateSalesContract(
+                    filePath: $@"{Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Документы по сделкам")}\{dealId}_Документ_купли_продажи.docx",
+                    buyer: db.getValuesFromColumn($"SELECT concat(clients.surname, ' ', clients.name, ' ', clients.patronymic) FROM db17.deals join clients on deals.client=clients.id where dealId={dealId};")[0],
+                    seller: db.getValuesFromColumn($"SELECT (select concat(surname,' ', name,' ', patronymic) from clients where id=object.owner_id) FROM db17.deals join object on deals.object=object.objectid where dealId={dealId};")[0],
+                    propertyDescription: $"{db.getValuesFromColumn($"SELECT (select type from object_type where id=object.object_type) FROM db17.deals join object on deals.object=object.objectid where dealid={dealId};")[0]} по адресу: {db.getValuesFromColumn($"SELECT object.object_address FROM db17.deals join object on deals.object=object.objectid where dealid={dealId};")[0]}",
+                    price: db.getIntValuesFromColumn($"SELECT object.price FROM db17.deals join object on deals.object=object.objectid where dealid={dealId};")[0],
+                    contractDate: db.getDateValuesFromColumn($"SELECT transaction_date FROM db17.deals where dealId={dealId};")[0]
+                    );
+                MessageBox.Show("Документ создан!");
+            } catch (Exception ex)
+            {
+                MessageStore.somethingWentWrongMessage();
+            }
         }
     }
 }
